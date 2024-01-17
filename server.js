04285a64433f0a2b37b520484fc83e0a913aca87
Steve Rep <steven.replogle1@gmail.com>
2024-01-17 10:37:54 -0800

    mayanwolfe full stack template CRUD EJS MongoDB

--- server.js
+++ server.js
@@ -13,23 +13,67 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MO
  .then(() => console.log('MongoDB Connected'))
  .catch(err => console.log(err))
 
-//Middleware
-
-// enable CORS
-app.use(cors())
+// Enable CORS
+app.use(cors());
 
 // Serve static files
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Parse requests
-app.use(express.urlencoded({extended: false}))
+// Parse requests for strings/arrays, use extended: true for nested requests
+app.use(express.urlencoded({ extended: false }));
+
+//Parse JSON requests
 app.use(express.json())
 
 // Set EJS as templating engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-// LOGIC GOES HERE
+// Routes
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
+
+app.get('/item', async (req, res) => {
+  const items = await Item.find({});
+  res.render('item', { items });
+});
+
+// Create
+app.post('/item', async (req, res) => {
+  const newItem = new Item(req.body);
+  try {
+    await newItem.save();
+    res.redirect('/item');
+  } catch (err) {
+    //you can set up custom error handling on the client side using this information
+    res.redirect('/item?error=true');
+  }
+});
+
+// Update
+app.post('/item/update/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  try {
+    await Item.findByIdAndUpdate(id, { name, description });
+    res.redirect('/item');
+  } catch (err) {
+    //you can set up custom error handling on the client side using this information
+    res.redirect(`/item?error=true`);
+  }
+});
+
+// Delete
+app.delete('/item/delete/:id', async (req, res) => {
+  const { id } = req.params;
+  await Item.findByIdAndDelete(id);
+  //Send success back to the client-side function
+  res.status(200).json({ message: 'Item deleted successfully' });
+});
+
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on: http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
